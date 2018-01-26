bfb0717d0a3840a8d3cbb9f3ebb60f63befd8d92
DK <dkainama@jouwomgeving.nl>
2018-01-26 16:36:33 +0100

    modified transactions

--- hooked/transactions.py
+++ hooked/transactions.py
@@ -2,6 +2,10 @@
 
 from enum import Enum
 
+# from django.contrib.postgres.fields import JSONField
+from django.db import models
+from django.utils import timezone
+
 
 class TransactionStatus(Enum):
     AWAITING = 1
@@ -13,5 +17,25 @@ class TransactionStatus(Enum):
         return "%s" % self.value
 
 
-class HookedTransaction:
+class WebHookTransaction(models.Model):
+    
     STATUS = TransactionStatus
+    
+    # request body & meta
+    body = models.TextField()
+    meta = models.TextField()
+    
+    # status of transaction { TransactionStatus }
+    status = models.IntegerField(choices=STATUS, default=STATUS.AWAITING.value)
+    
+    # flags
+    modified = models.DateTimeField()
+    created = models.DateTimeField(default=timezone.now())
+    
+    
+    @property
+    def current_status(self):
+        TransactionStatus(self.status)
+    
+    def __unicode__(self):
+        return u'{0}'.format(self.date_event_generated)
--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from django.db import models
 from django.test import TestCase
 
-from hooked.transactions import HookedTransaction, TransactionStatus
-
-
-class Message(models.Model, HookedTransaction):
-    pass
+from hooked.transactions import TransactionStatus, WebHookTransaction
 
 
 class TestTransactionsOnModel(TestCase):
-    def test_extending_message_with_transaction_mixin(self):
-        self.assertEquals(
-            HookedTransaction.STATUS.AWAITING,
-            TransactionStatus.AWAITING)
-        self.assertEquals(
-            HookedTransaction.STATUS.PROGRESS,
-            TransactionStatus.PROGRESS)
-        self.assertEquals(
-            HookedTransaction.STATUS.PROCESSED,
-            TransactionStatus.PROCESSED)
-        self.assertEquals(
-            HookedTransaction.STATUS.FAILED,
-            TransactionStatus.FAILED)
+    def test_check_transaction_enum_status(self):
+        self.assertEquals(WebHookTransaction.STATUS.AWAITING, TransactionStatus.AWAITING)
+        self.assertEquals(WebHookTransaction.STATUS.PROGRESS, TransactionStatus.PROGRESS)
+        self.assertEquals(WebHookTransaction.STATUS.PROCESSED, TransactionStatus.PROCESSED)
+        self.assertEquals(WebHookTransaction.STATUS.FAILED, TransactionStatus.FAILED)
+        
+        
+    def test_init_transaction_hook(self):
+        # setup hook
+        hook = WebHookTransaction()
+        # asserts
+        self.assertEquals(hook.current_status, TransactionStatus.AWAITING)
+
+
+    def test_body_and_meta_transaction_hook(self):
+        # setup hook
+        hook = WebHookTransaction()
+        # set request data
+        hook.body = {'hooked': True }
+        hook.meta = {'user-agent': 'pirateOS/0.1'}
+        
+        # save
+        hook.save()
+        
+        # asserts
+        self.assertEquals(hook.current_status, TransactionStatus.AWAITING)
+        self.assertEquals(hook.body, {'hooked': True })
+        self.assertEquals(hook.meta, "{'user-agent': 'pirateOS/0.1'}")
